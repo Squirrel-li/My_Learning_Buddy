fb72c42ca742013f6febc2e6c110c44f0f850f0d
lison <lison@SONGNB>
2025-11-16 23:34:51 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gb_all_s = new System.Windows.Forms.GroupBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.gb_0 = new System.Windows.Forms.GroupBox();
+            this.gb_0.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gb_all_s
+            // 
+            this.gb_all_s.Location = new System.Drawing.Point(93, 22);
+            this.gb_all_s.Name = "gb_all_s";
+            this.gb_all_s.Size = new System.Drawing.Size(169, 100);
+            this.gb_all_s.TabIndex = 0;
+            this.gb_all_s.TabStop = false;
+            this.gb_all_s.Text = "groupBox1";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.monthCalendar1.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.monthCalendar1.Location = new System.Drawing.Point(12, 469);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 1;
+            this.monthCalendar1.Visible = false;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(162, 175);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(6, 22);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(140, 620);
+            this.flowLayoutPanel1.TabIndex = 8;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // gb_0
+            // 
+            this.gb_0.Controls.Add(this.flowLayoutPanel1);
+            this.gb_0.Font = new System.Drawing.Font("標楷體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.gb_0.Location = new System.Drawing.Point(268, 12);
+            this.gb_0.Name = "gb_0";
+            this.gb_0.Size = new System.Drawing.Size(984, 647);
+            this.gb_0.TabIndex = 1;
+            this.gb_0.TabStop = false;
+            this.gb_0.Text = "today";
+            this.gb_0.Enter += new System.EventHandler(this.gb_0_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.gb_0);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.gb_all_s);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gb_0.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gb_all_s;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.GroupBox gb_0;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project
+{
+    public partial class Form1 : Form
+    {
+        String[] table_suject;
+        String[] table_classification;
+        public Form1()
+        {
+            InitializeComponent();
+            init_form();
+        }
+        class TASK
+        {
+            public TASK(UInt16 suject, UInt16 classification, String task_describe, DateTime deadline)
+            {
+                this.suject = suject;
+                this.classification = classification;
+                this.task_describe = task_describe;
+                this.deadline = deadline;
+            }
+            public UInt16 suject;
+            public UInt16 classification;
+            public String task_describe = "";
+            public DateTime deadline;
+            public bool done = false;
+        }
+        class TB_TASK : TextBox
+        {
+            public TB_TASK(FlowLayoutPanel contain_flpanel, TASK input)
+            {
+                this.Width = contain_flpanel.Width - 20 - (contain_flpanel.Margin.Left + contain_flpanel.Margin.Right);
+                this.BorderStyle = BorderStyle.None;
+
+                //this.ScrollBars = ScrollBars.Vertical;
+                this.Multiline = true;
+                this.TextAlign = HorizontalAlignment.Center;
+                this.Enabled = false;
+                this.Cursor = Cursors.Default;
+                this.ForeColor = Color.Black;
+
+                String text_index = tex_tset(input);
+
+                this.ReadOnly = true;
+                this.Font = new Font("標楷體", 16);
+                this.Text = text_index;
+                this.Height = this.FontHeight * 3 + 2;
+            }
+
+            private String tex_tset(TASK input)
+            {
+                String result = "";
+                result = $"{input.suject}\r\n{input.classification}\r\n{input.task_describe}";
+                return result;
+            }
+        }
+
+        private void init_form()
+        {
+            this.table_suject = new String[] { "計算機結構", "視窗程式設計" };
+            this.table_suject = new String[] { "計算機結構", "視窗程式設計" };
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void gb_0_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                String index = $"{i}\r\ntest {i}\r\ntest {i}";
+                Task new_task = new Task(0, 0, "test");
+                TB_TASK btn = new TB_TASK(flowLayoutPanel1, new_task);
+                flowLayoutPanel1.Controls.Add(btn);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
